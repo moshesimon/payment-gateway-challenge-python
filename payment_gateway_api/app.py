687b19b3332e829d60c3b3c302070eb5bdffc56b
Moshe Simon <moshesimon@outlook.com>
2024-06-03 20:13:01 +0100

    added comments

--- payment_gateway_api/app.py
+++ payment_gateway_api/app.py
@@ -1,10 +1,72 @@
 from typing import Dict
-from fastapi import FastAPI
-
+from fastapi import FastAPI, HTTPException, Request, Header
+import requests
+import ipdb
+import uuid
 
 app = FastAPI()
 
+def validate_request(request):
+    # validate request
+    return True
 
 @app.get("/")
 async def ping() -> Dict[str, str]:
     return {"app": "payment-gateway-api"}
+
+@app.post("/payment")
+async def payment(request: Request, authorization: str = Header(...)):
+    # check authorization header
+    # decode the token to get the merchant_id
+
+    data = await request.json()
+
+    # check if similar request has been made recently from the same user
+    # if yes, return the previous payment record
+    # else, store the payment record and proceed with the payment process
+
+    # (don't remember what else you asked me to write comments on sorry!)
+    
+    if validate_request(data):
+        print("Request is valid")
+        
+        json = marshal_aquirer_payload(data)
+
+        response = requests.post("http://localhost:8080/payments",json=json, headers={"Content-Type": "application/json"})
+        
+        if response.status_code != 200: # not all error codes are 200
+            raise HTTPException(status_code=400, detail="Bank generated an error")
+
+        bank_response = response.json()
+        payment_status = "Authorized" if bank_response["authorized"] else "Declined"
+    else:
+        payment_status = "Rejected"
+
+    
+    payment_id = str(uuid.uuid4())
+    payment_record = {
+        "id": payment_id,
+        "card_number": data["card_number"][-4:],
+        "amount": data["amount"],
+        "currency": data["currency"],
+        "status": payment_status,
+        "expiry_month": data["expiry_month"],
+        "expiry_year": data["expiry_year"],
+    }
+    
+    # updated the payment record with the payment status
+
+    return payment_record
+
+def marshal_aquirer_payload(data):
+    return {
+        "card_number": data["card_number"],
+        "cvv": data["cvv"],
+        "expiry_date": f'{data["expiry_month"]:02}/{data["expiry_year"]}',
+        "amount": data["amount"],
+        "currency": data["currency"]
+    }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8081)
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,11 +1,84 @@
 from fastapi.testclient import TestClient
-from payment_gateway_api.app import app
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from payment_gateway_api.app import app, marshal_aquirer_payload
 
+client = TestClient(app)
 def test_example():
-    client = TestClient(app)
 
     response = client.get("/")
 
     assert response.status_code == 200
     assert response.json() == {"app": "payment-gateway-api"}
+
+def test_payment_Rejected():
+
+    response = client.post("/payment", json={
+        "card_number": "2222405343277",
+        "cvv": "123",
+        "expiry_month": 12,
+        "expiry_year": 2023,
+        "amount": 100,
+        "currency": "USD"
+    })
+
+    assert response.status_code == 200
+    assert response.json()['status'] == 'Rejected'
+
+def test_payment_Declined():
+
+    response = client.post("/payment", json={
+        "card_number": "2222405343248112",
+        "cvv": "456",
+        "expiry_month": 1,
+        "expiry_year": 2026,
+        "amount": 60000,
+        "currency": "USD"
+    })
+
+    assert response.status_code == 200
+    assert response.json()['status'] == 'Declined'
+
+def test_payment_Authorized():
+
+    response = client.post("/payment", json={
+        "card_number": "2222405343248877",
+        "cvv": "123",
+        "expiry_month": 4,
+        "expiry_year": 2025,
+        "amount": 100,
+        "currency": "GBP"
+    })
+
+    assert response.status_code == 200
+    assert response.json()['status'] == 'Authorized'
+
+def test_marshal_aquirer_payload():
+    data = {
+        "card_number": "2222405343248877",
+        "cvv": "123",
+        "expiry_month": 4,
+        "expiry_year": 2025,
+        "amount": 100,
+        "currency": "GBP"
+    }
+    assert marshal_aquirer_payload(data) == {
+        "card_number": "2222405343248877",
+        "expiry_date": "04/2025",
+        "currency": "GBP",
+        "amount": 100,
+        "cvv": "123"
+        }
+    
+def test_okay():
+    response = client.post("/payment")
+    print(response.json)
+    assert response.status_code == 200
+
+test_payment_Declined()
+# test_payment_Authorized()
+
+# test_payment_Rejected()
+# test_marshal_aquirer_payload()
